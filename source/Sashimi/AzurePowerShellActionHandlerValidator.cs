d2faa303cd1b524d5f1e4ae26b560f0645c8a0cb
Geoff Lamrock <geoff.lamrock@gmail.com>
2022-05-27 16:24:17 +1000

    Removes account validation for testing

--- source/Sashimi/AzurePowerShellActionHandlerValidator.cs
+++ source/Sashimi/AzurePowerShellActionHandlerValidator.cs
@@ -9,12 +9,12 @@ namespace Sashimi.AzureScripting
         public AzurePowerShellActionHandlerValidator()
         {
             // TODO: Don't want this rule to run when cloud connections are configured
-            When(a => a.ActionType == SpecialVariables.Action.Azure.ActionTypeName,
-                 () =>
-                 {
-                     RuleFor(a => a.Properties)
-                         .MustHaveProperty(SpecialVariables.Action.Azure.AccountId, "Please select an Account or provide a variable expression for the Account ID to use.");
-                 });
+            //When(a => a.ActionType == SpecialVariables.Action.Azure.ActionTypeName,
+            //     () =>
+            //     {
+            //         RuleFor(a => a.Properties)
+            //             .MustHaveProperty(SpecialVariables.Action.Azure.AccountId, "Please select an Account or provide a variable expression for the Account ID to use.");
+            //     });
         }
     }
 }
